4445abd474f22afba7208138dfcf5a105ec75c04
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-07-02 09:47:30 -0400

    add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :mods_categories
+    has_many :mods, through: :mods_categories
 end
--- app/models/mod.rb
+++ app/models/mod.rb
@@ -1,3 +1,9 @@
 class Mod < ApplicationRecord
   belongs_to :game
+  has_many :comments
+  has_many :commenters, through: :comments, source: :user
+  has_many :users_downloads
+  has_many :downloads, through: :users_downloads, source: :user
+  has_many :mods_categories
+  has_many :categories, through: :mods_categories
 end
