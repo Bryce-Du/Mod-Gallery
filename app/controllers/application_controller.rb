6dfd9c471193f22e0ac03fe1fa6765c76f0128f1
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-07-05 08:14:47 -0400

    continue login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     private
     def current_user
-        if session[:user_id] ? User.find_by(id: session[:user_id]) : nil
+        session[:user_id] ? User.find_by(id: session[:user_id]) : nil
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            redirect_to "/signup"
+        end
     end
     def show
     end
