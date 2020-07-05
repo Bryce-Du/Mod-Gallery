6a87e16366d1f5c17afee8a310fb434ea3b920db
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-07-05 07:00:34 -0400

    continue login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
         render "/users/new"
     end
     def create
+        # logs the user in if their password authenticates and there is a user with that name
+        # redirects with errors if username not found or password incorrect
+        if @user = User.find_by(username: params[:user][:username])
+            #log them in
+        else
+            render "/users/new"
+        end
     end
     def destroy
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
     has_many :comments
     # has_many :commented_mods, through: :comments, source: :mod # really shouldn't need to access this, you'd want to see which users have commented on a mod, but not which mods a user has commented on
     has_many :creations, class_name: "Mod"
+
+    validates :username, presence: true
+    validates :password, presence: true
+    validates :password, length: {in: 8..16}
 end
