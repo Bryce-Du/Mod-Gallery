9a3e94b0128d66cdce6097c34dcc4c7f5b2adf3c
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-07-06 13:08:29 -0400

    continue omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github-oauth2, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_SECRET']
+    provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_SECRET']
 end
