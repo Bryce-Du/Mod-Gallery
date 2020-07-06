c76e7e9e93756b9cf73a17265187e76dbe71816e
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-07-06 10:04:21 -0400

    make layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,6 @@ class SessionsController < ApplicationController
         @user = User.new
     end
     def create
-        # logs the user in if their password authenticates and there is a user with that name
-        # redirects with errors if username not found or password incorrect
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
@@ -13,9 +11,11 @@ class SessionsController < ApplicationController
             render "/sessions/new"
         end
     end
+    def main
+        @user = current_user
+    end
     def destroy
         session.delete(:user_id)
         redirect_to root_path
     end
-
 end
