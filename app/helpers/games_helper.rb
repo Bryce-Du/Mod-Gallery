012dc137f7ad09ce061df64838af84aa02f7a69d
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-07-07 14:49:59 -0400

    update Sidebar

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,12 @@
 module GamesHelper
+    def selected_game
+        if params[:controller] == "games" && params[:id]
+            Game.find(params[:id])
+        elsif params[:game_id]
+            Game.find(params[:game_id])
+        end
+    end
+    def selected_game_name
+        !!selected_game ? selected_game.name : ""
+    end
 end
